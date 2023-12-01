b959789c58988882b1be7fa7980235a0f78fc885
kacperjagiela <jagielakacper80@gmail.com>
2023-12-01 22:15:53 +0100

    feat: 1st day 1st part

---
+++ lib/001_Trebuchet/001.ts
@@ -0,0 +1,36 @@
+import fs from "fs";
+
+const input = fs.readFileSync("./inputs/001_input.txt", "utf8");
+
+const lines = input.trim().split("\n");
+
+const numbers: number[] = [];
+
+for (let i = 0; i < lines.length; i++) {
+  const line = lines[i];
+  const letters = line.split("");
+  let firstNumber: number;
+  let secondNumber: number;
+
+  for (let j = 0; j < letters.length; j++) {
+    if (Number.isInteger(parseInt(letters[j]))) {
+      firstNumber = parseInt(letters[j]);
+      break;
+    }
+  }
+
+  for (let j = letters.length - 1; j >= 0; j--) {
+    if (Number.isInteger(parseInt(letters[j]))) {
+      secondNumber = parseInt(letters[j]);
+      break;
+    }
+  }
+
+  numbers.push(parseInt(`${firstNumber}${secondNumber}`));
+}
+
+numbers.forEach((number, index) => console.log(index, number));
+
+const sum = numbers.reduce((a, b) => a + b, 0);
+
+console.log(sum);
